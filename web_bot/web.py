83ebfe8f9921c5ddf539ff41427bc5b03ab1a650
slatt <u1323656@utah.edu>
2025-02-23 20:33:29 -0700

    grabs claim button, needs logic for timer / retry if needed

--- web_bot/web.py
+++ web_bot/web.py
@@ -21,14 +21,20 @@ if session_value:
 # Refresh the page to apply cookies
 driver.refresh()
 
+# Ensures components are loaded
+time.sleep(3)
+
 # Find Submit
 # Locate the button based on the text inside the div
 
+form_element = driver.find_element(By.CSS_SELECTOR, "form.svelte-188ba96")
+# attempts to grab numbers left until daily claim
+# nums = driver.find_element(By.CLASS_NAME, "digits svelte-1gvsbmk")
+submit = form_element.find_element(By.CSS_SELECTOR, "button[type='submit'][data-test='password-reset-button'] div.contents")
 
-
-# if submit.is_enabled():
-#     print("The button is clickable.")
-# else:
-#     print("The button is disabled.")
+if submit.is_enabled():
+    print("The button is clickable.")
+else:
+    print("The button is disabled.")
 
 time.sleep(1000)
